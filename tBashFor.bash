0cc9e185984176e3b6819f6fadb0df334b10a663
Paul Houghton <paul4hough@gmail.com>
2015-05-01 10:17:52 -0700

    base for loop w/ every other alpha a-b c-d e-f ...

---
+++ tBashFor.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+dcnt=22 # disk count
+d=0
+fd=a
+
+while test $d -lt $dcnt ; do
+
+  let d=d+2;
+  sd=$(echo "$fd" | tr "0-9a-z" "1-9a-z_")
+
+  echo $d $fd $sd
+
+  fd=$(echo "$sd" | tr "0-9a-z" "1-9a-z_")
+done
